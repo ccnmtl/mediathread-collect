d6d576901cb47d5909f28e1a3fe0d3f504ed6ee3
Nik Nyby <nikolas@gnu.org>
2017-04-24 17:52:42 -0400

    api fixes for jsdom 10.0.0

--- tests/host-handler.js
+++ tests/host-handler.js
@@ -7,9 +7,6 @@ const { JSDOM } = jsdom;
 const HH = require('../src/host-handler.js');
 const { hostHandler } = HH;
 
-const jqueryUrl = 'https://ajax.googleapis.com/ajax/libs/jquery/' +
-    '2.1.4/jquery.min.js';
-
 describe('hosthandler', function() {
     it('should exist', function() {
         assert.equal(typeof hostHandler, 'object');
@@ -17,104 +14,58 @@ describe('hosthandler', function() {
     });
 });
 
-describe('library.artstor.org', function() {
-    it('has the expected JavaScript', function(done) {
-        new JSDOM('', {
-            url: 'http://library.artstor.org/library/',
-            resourceLoader: function(resource, callback) {
-                // Only load relevant scripts, otherwise jsdom times out.
-                if (resource.url.hostname === 'library.artstor.org') {
-                    resource.defaultFetch(callback);
-                } else {
-                    callback();
-                }
-            },
-            features: {
-                FetchExternalResources: ['script'],
-                ProcessExternalResources: ['script']
-            },
-            'done': function(err, window) {
-                assert.ok(typeof window.dijit.registry.length === 'number');
-                assert.ok(typeof window.dijit.registry._hash === 'object');
-                done();
-            }
-        });
-    });
-});
-
 describe('blakearchive.org', function() {
     it('has the expected title', function(done) {
-        new JSDOM('', {
-            url: 'http://www.blakearchive.org/' +
-                'copy/songsie.n?descId=songsie.n.illbk.08',
-            scripts: [jqueryUrl],
-            done: function(err, window) {
-                var $ = window.$;
-                assert.equal($('title').text(), 'The William Blake Archive');
-                done();
-            }
-        });
+        JSDOM.fromURL(
+            'http://www.blakearchive.org/' +
+                'copy/songsie.n?descId=songsie.n.illbk.08').then(dom => {
+                    assert.equal(dom.window.document.title,
+                                 'The William Blake Archive');
+                    done();
+                });
     });
 
     it('has the expected title for the enlarged image page', function(done) {
-        new JSDOM('', {
-            url: 'http://www.blakearchive.org/' +
-                'new-window/enlargement/songsie.n?descId=songsie.n.illbk.08',
-            scripts: [jqueryUrl],
-            done: function(err, window) {
-                var $ = window.$;
-                assert.equal($('title').text(), 'The William Blake Archive');
+        JSDOM.fromURL('http://www.blakearchive.org/' +
+                      'new-window/enlargement/songsie.n?' +
+                      'descId=songsie.n.illbk.08')
+            .then(dom => {
+                assert.equal(dom.window.document.title, 'The William Blake Archive');
                 done();
-            }
-        });
+            });
     });
 });
 
 describe('classpop.ccnmtl.columbia.edu', function() {
     it('has the expected DOM', function(done) {
-        new JSDOM('', {
-            url: 'https://classpop.ccnmtl.columbia.edu/' +
-                'content/perspectives-freedom-speech',
-            scripts: [jqueryUrl],
-            done: function(err, window) {
-                if (typeof window !== 'undefined') {
-                    var $ = window.$;
-                    assert.ok($('#currently_playing').length === 1);
-                }
+        JSDOM.fromURL('https://classpop.ccnmtl.columbia.edu/' +
+                      'content/perspectives-freedom-speech')
+            .then(dom => {
+                assert.ok(dom.window.document.getElementById('currently_playing'));
                 done();
-            }
-        });
+            });
     });
 });
 
 describe('flickr.com', function() {
     it('has the expected DOM', function(done) {
-        new JSDOM('', {
-            url: 'https://www.flickr.com/photos/dropacat/15183956471/',
-            scripts: [jqueryUrl],
-            done: function(err, window) {
-                var $ = window.$;
-                assert.ok($('img').length > 0);
+        JSDOM.fromURL('https://www.flickr.com/photos/dropacat/15183956471/')
+            .then(dom => {
+                assert.ok(dom.window.document.getElementsByTagName('img').length > 0);
                 done();
-            }
-        });
+            });
     });
 });
 
 describe('youtube.com', function() {
     it('has the expected DOM', function(done) {
-        new JSDOM('', {
-            url: 'https://www.youtube.com/watch?v=ZQhbB6-cT3Y',
-            scripts: [jqueryUrl],
-            done: function(err, window) {
-                var $ = window.$;
-
+        JSDOM.fromURL('https://www.youtube.com/watch?v=ZQhbB6-cT3Y')
+            .then(dom => {
                 // The host handler looks for #movie_player, but this won't
                 // get rendered in jsdom. We can look for the on-page
                 // javascript that instantiates it with this ID, though.
-                assert.ok($('script').text().match(/movie_player/));
+                assert.ok(dom.serialize().indexOf('movie_player') >= 0);
                 done();
-            }
-        });
+            });
     });
 });
